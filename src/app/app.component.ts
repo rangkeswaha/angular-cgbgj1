1b613a978b48605f45d60f19c349ea5648381791
rangkeswaha <c14180243@john.petra.ac.id>
2020-04-15 11:40:39 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,25 +8,37 @@ import { Component } from '@angular/core';
 export class AppComponent  {
   name = 'Angular';
 
-  // angka1 =(document.getElementById("txt1")as HTMLInputElement).valueAsNumber;
-  // angka2 =(document.getElementById("txt2")as HTMLInputElement).valueAsNumber;
 
-  angka1 = parseInt("");
-  angka2 = parseInt("");
+  // angka1 = parseInt("");
+  // angka2 = parseInt("");
+  angka1 : number;
+  angka2 : number;
+  hasil : number;
+  cek : boolean = true;
 
-  angka3 = Math.pow(this.angka1, this.angka2);
-  angka4 = this.angka3 % 2;
+  // angka3 = Math.pow(this.angka1, this.angka2);
+  // angka4 = this.angka3 % 2;
 
-  ganjil(){
-    var hasil = Math.pow(this.angka1, this.angka2);
-    alert(hasil);   
-  }
+  // ganjil(){
+  //   var hasil = Math.pow(this.angka1, this.angka2);
+  //   alert(hasil);   
+  // }
 
 
   polaB = ["***","****","*****","******","*******"];
   PolaA = ["*****0","****00","***000","**","*"];
-
-  kilkButton(){
+  
+  KlikButton()
+  {
+    this.hasil = 1;
+    this.angka1 = parseInt((document.getElementById("a")as HTMLInputElement).value);
+    this.angka2 = parseInt((document.getElementById("b")as HTMLInputElement).value);
     
+    this.hasil = Math.pow(this.angka1,this.angka2);
+
+    if(this.hasil % 2 == 0)
+    {
+      this.cek = false;
+    }
   }
 }
