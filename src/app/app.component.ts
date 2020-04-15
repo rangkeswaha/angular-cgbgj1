874bbbf874251b9c3c5e21def46a89f1b40d469a
rangkeswaha <c14180243@john.petra.ac.id>
2020-04-15 10:23:20 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+
+  angka1 = parseInt("");
+  angka2 = parseInt("");
+
+  angka3 = Math.pow(this.angka1, this.angka2);
+  angka4 = this.angka3 % 2;
+
+  polaB = ["***","****","*****","******","*******"];
+  PolaA = ["*****0","****00","***000","**","*"];
+
+  kilkButton(){
+    
+  }
 }
