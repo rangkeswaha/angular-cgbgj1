cac060f4b75858830054434a6de30afffaa3ba3f
rangkeswaha <c14180243@john.petra.ac.id>
2020-04-15 11:25:44 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,20 @@ import { Component } from '@angular/core';
 export class AppComponent  {
   name = 'Angular';
 
+  // angka1 =(document.getElementById("txt1")as HTMLInputElement).valueAsNumber;
+  // angka2 =(document.getElementById("txt2")as HTMLInputElement).valueAsNumber;
+
   angka1 = parseInt("");
   angka2 = parseInt("");
 
   angka3 = Math.pow(this.angka1, this.angka2);
   angka4 = this.angka3 % 2;
+
+  ganjil(){
+    var hasil = Math.pow(this.angka1, this.angka2);
+    alert(hasil);   
+  }
+
 
   polaB = ["***","****","*****","******","*******"];
   PolaA = ["*****0","****00","***000","**","*"];
